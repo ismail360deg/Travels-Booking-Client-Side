6c3e78514db6cb87411fa80e08aeb7f8fc84f688
ismail360deg <ismailbhuiyan2000@gmail.com>
2023-02-14 21:51:07 +0600

    first commit

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './../Header/Header';
+import Routers from '../../router/Routers';
+import Footer from './../Footer/Footer';
+
+const Layout = () => {
+  return (
+    <>
+    <Header></Header>
+    <Routers></Routers>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default Layout
---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Newsletter from '../shared/Newsletter'
+
+const About = () => {
+  return (
+    <>
+    <Newsletter></Newsletter>
+    </>
+  )
+}
+
+export default About
